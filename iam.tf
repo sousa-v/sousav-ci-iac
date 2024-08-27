8d4ea210d88d9410cdf25ee11bc0c3010bd2daee
Victor Sousa <victorbrito001@gmail.com>
2024-08-27 10:43:47 -0300

    new: test new role

--- iam.tf
+++ iam.tf
@@ -12,6 +12,33 @@ resource "aws_iam_openid_connect_provider" "oidc_git" {
   thumbprint_list = ["d89e3bd43d5d909b47a18977aa9d5ce36cee184c"]
 }
 
+resource "aws_iam_role" "tf-role" {
+  name = "tf-role"
+
+  assume_role_policy = jsonencode({
+    Version : "2012-10-17",
+    Statement : [
+      {
+        Effect : "Allow",
+        Action : "sts:AssumeRoleWithWebIdentity",
+        Principal : {
+          Federated : "arn:aws:iam::908027382789:oidc-provider/token.actions.githubusercontent.com"
+        },
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = ["sts.amazonaws.com"]
+            "token.actions.githubusercontent.com:sub" = ["repo:sousa-v/sousav-ci-iac:ref:refs/heads/main"]
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    IAC = "True"
+  }
+}
+
 resource "aws_iam_role" "app-runner-role" {
   name = "app-runner-role"
 
